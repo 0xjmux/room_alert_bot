926f1e90b7f4d115227db0caa1e56c376eedc169
Jacob <jb@jacobbokor.com>
2021-11-12 21:37:10 -0800

    added basic discord bot code to file, commented out

--- is_ieee_open.py
+++ is_ieee_open.py
@@ -1,17 +1,38 @@
+#!/usr/bin/env python3
+########################################
+# is_ieee_open.py
+# UCI IEEE, Nov 2021
+#
+# Python program written for UCI IEEE 
+#  to show whether the room is open or not 
+#  in the club's Discord server. 
+########################################
+
 import RPi.GPIO as GPIO
 import time
 import os
+import discord
+import _creds_ # creds is separate file used to store api secrets
 
-#adjust for where your switch is connected
-buttonPin = 17 #change this later
+# variable defs
+switch_pin = 17 
 R_LED = 27
 G_LED = 22
+
+#initialize gpio
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buttonPin,GPIO.IN)
 GPIO.setup(R_LED,GPIO.OUT)
 GPIO.setup(G_LED,GPIO.OUT)
 
 prev_input = 0
+
+client = discord.Client()
+
+#@client.event
+#async def on_ready():
+#    print('We have logged in as {0.user}'.format(client))
+
 
 try:
     while True:
@@ -37,3 +58,16 @@ except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt
 finally:
     print("clean up")
     GPIO.cleanup()
+   
+
+
+
+#@client.event
+#async def on_message(message):
+#    if message.author == client.user:
+#        return
+#
+#    if message.content.startswith('$hello'):
+#        await message.channel.send('Hello!')
+#
+#client.run(_creds_.TOKEN)
